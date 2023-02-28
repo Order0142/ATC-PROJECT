c86fd51407075393cf433850923d3c8df8b93e05
Adewale <adewalejnr0142@gmail.com>
2023-02-28 23:08:01 +0100

    update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,22 +1,41 @@
 import React from "react";
+import classes from "./footer.module.css";
 import Flogo from "../../asset/image/flogo.svg";
 
 const Footer = () => {
   return (
-    <footer className="{classes.main}">
-      <div>
-        <img src={Flogo} alt="" />
-        <h3>Trafalgar</h3>
-        <p>
-          Trafalgar provides progressive, and affordable healthcare, accessible
-          on mobile and online for everyone.
-        </p>
-        <p>©Trafalgar PTY LTD 2020. All rights reserved</p>
-      </div>
-      <div>
-        <h3>Company</h3>
-        <p>About</p>
-        <p>Testimonials</p>
+    <footer className={classes.main}>
+      <div className={classes.footer}>
+        <div>
+          <img src={Flogo} alt="" />
+          <p>
+            Trafalgar provides progressive, and affordable
+            <br /> healthcare, accessible on mobile and online
+            <br /> for everyone.
+          </p>
+          <p>©Trafalgar PTY LTD 2020. All rights reserved</p>
+        </div>
+        <div>
+          <h3>Company</h3>
+          <p>About</p>
+          <p>Testimonials</p>
+          <p>Find a doctor</p>
+          <p>App</p>
+        </div>
+        <div>
+          <h3>Regions</h3>
+          <p>Indonesia</p>
+          <p>Singapore</p>
+          <p>Hongkong</p>
+          <p>Canada</p>
+        </div>
+        <div>
+          <h3>Help</h3>
+          <p>Help center</p>
+          <p>Contact support</p>
+          <p>Instructions</p>
+          <p>How it works</p>
+        </div>
       </div>
     </footer>
   );
