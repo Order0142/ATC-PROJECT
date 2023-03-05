87f68080d311ec5386a3459726c8cd771653489d
Adewale <adewalejnr0142@gmail.com>
2023-03-05 23:12:56 +0100

    update

--- src/components/landing/hero/hero.js
+++ src/components/landing/hero/hero.js
@@ -3,14 +3,15 @@ import HeroImage from "../../../asset/image/heroImage.png"
 
 const Hero = () => {
   return (
-    <div>
-      <div className={classes.main}>
+    <div className={classes.main}>
       <div className={classes.hero}>
+      <div className={classes.text}>
         <h3>Virtual healthcare for you</h3>
         <p>
           Trafalgar provides progressive, and affordable healthcare, accessible
           on mobile and online for everyone
         </p>
+        <button>Consult today</button>
       </div>
       <div>
         <img src={HeroImage} alt="" />
