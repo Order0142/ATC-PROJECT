a5d10fc4985026029641051886be06662080dda3
Adewale <adewalejnr0142@gmail.com>
2023-03-08 14:57:35 +0100

    update

---
+++ src/components/landing/healthcare/healthcare.js
@@ -0,0 +1,25 @@
+import classes from "./healthcare.module.css";
+
+const Healthcare = () => {
+  return (
+    <div className={classes.main}>
+      <div className={classes.healthcare}>
+        <div>
+          <img src={null} alt="" />
+        </div>
+        <div>
+          <h3>Leading healthcare providers</h3>
+          <span></span>
+          <p>
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone. To us, it’s not just
+            work. We take pride in the solutions we deliver
+          </p>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Healthcare;
--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,12 +1,14 @@
 import Carousel from "../../components/landing/carousel/carousel";
 import Hero from "../../components/landing/hero/hero";
 import OurService from "../../components/landing/ourService/ourService";
+import Healthcare from "../../components/landing/healthcare/healthcare"
 
 const Landing = () => {
   return (
     <div>
       <Hero />
       <OurService />
+      <Healthcare />
       <Carousel />
     </div>
   );
