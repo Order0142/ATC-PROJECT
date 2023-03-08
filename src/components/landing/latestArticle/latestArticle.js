6e3da1f255a786a83a78acca9193c4e56583f9fc
Adewale <adewalejnr0142@gmail.com>
2023-03-08 17:07:33 +0100

    update

---
+++ src/components/landing/latestArticle/latestArticle.js
@@ -0,0 +1,15 @@
+import classes from "./latestArticle.module.css";
+
+const LatestArticle = () => {
+  return (
+    <div className={classes.main}>
+      <div className={classes.article}>
+        <div className={classes.articleContent}>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default L;
