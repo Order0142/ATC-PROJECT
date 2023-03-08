1b8ccd2096d397494ac61c9a73295d66735a569f
Adewale <adewalejnr0142@gmail.com>
2023-03-08 15:44:42 +0100

    update

---
+++ src/components/landing/moblileapp/mobileapp.js
@@ -0,0 +1,11 @@
+import classes from "./mobileapp.module.css";
+
+const Mobileapp = () => {
+  return (
+    <div className={classes.main}>
+      <div className={classes.mobileapp}></div>
+    </div>
+  );
+};
+
+export default Mobileapp;
