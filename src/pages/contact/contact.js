014b597fd7629e8b48d8cb2abc633a299fd8b05b
Adewale <adewalejnr0142@gmail.com>
2023-03-15 14:40:16 +0100

    contact

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <div>
+        <div>
+          <h2>welcome to our community</h2>
+          <p>Create account and connect with Plantians</p>
+          <img src={null} alt="" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
