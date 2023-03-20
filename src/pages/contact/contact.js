9fe6d30a5b8b6552714a089ded54b762ee8ff573
Adewale <adewalejnr0142@gmail.com>
2023-03-20 15:12:15 +0100

    contact

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,14 +1,19 @@
 import classes from "./contact.module.css";
 import { Link } from "react-router-dom";
-import Button from "../../components/button/button";
-import contactImg from "../../asset/image/contactImg.png"
+import contactImg from "../../asset/image/contactImg.png";
+import Google from "../../asset/image/Google.svg";
+import Facebook from "../../asset/image/Facebook.svg";
 
 const Contact = () => {
   return (
     <div className={classes.main}>
       <div className={classes.contact}>
         <div className={classes.imgSec}>
-          <h1>Welcome to<br />our community</h1>
+          <h1>
+            Welcome to
+            <br />
+            our community
+          </h1>
           <p>Create account and connect with Plantians</p>
           <img src={contactImg} alt="" />
         </div>
@@ -24,12 +29,32 @@ const Contact = () => {
           </div>
           <div className={classes.signUp}>
             <span>
-              <Button title="Sign up with Google" />
+              <button>
+                <img src={Google} alt="" />
+                Sign up with Google
+              </button>
             </span>
             <span>
-              <Button title="Sign up with Facebook" />
+              <button>
+                <img src={Facebook} alt="" />
+                Sign up with Facebook
+              </button>
             </span>
             <p>Or</p>
+            <form>
+              <div>
+                <label>Name</label>
+                <input type="text" placeholder="Name" />
+              </div>
+              <div>
+                <label>Email</label>
+                <input type="text" placeholder="Email" />
+              </div>
+              <div>
+                <label>Name</label>
+                <input type="password" placeholder="Password" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
