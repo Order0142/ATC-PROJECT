94f594b7d6e649df48ee0878073649aa941b5523
Adewale <adewalejnr0142@gmail.com>
2023-03-30 21:37:09 +0100

    mobile

--- src/components/landing/moblileapp/mobileapp.js
+++ src/components/landing/moblileapp/mobileapp.js
@@ -1,20 +1,22 @@
 import classes from "./mobileapp.module.css";
 import Button from "../../button/button";
-import SecImg3 from "../../../asset/image/secImg3.png"
+import SecImg3 from "../../../asset/image/secImg3.png";
 
 const Mobileapp = () => {
   return (
     <div className={classes.main}>
       <div className={classes.mobileapp}>
         <div className={classes.text}>
-          <h3>Download our mobile apps</h3>
+          <h3>Download our mobile apps.</h3>
           <span></span>
           <p>
             Our dedicated patient engagement app and web portal allow you to
             access information instantaneously (no tedeous form, long calls, or
-            administrative hassle) and securely
+            administrative hassle) and securely.
           </p>
-          <Button title="Download &#129047;" />
+          <div className={classes.btn}>
+            <Button title="Download &#129047;" />
+          </div>
         </div>
         <div>
           <img src={SecImg3} alt="" />
